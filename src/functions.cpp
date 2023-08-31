e35275f4f55ac6a075e5e258a59ce9786cfe7c0f
ericcardozo <eric.m.cardozo@mi.unc.edu.ar>
2023-08-31 13:50:34 -0300

    added softmax

--- src/functions.cpp
+++ src/functions.cpp
@@ -4,6 +4,7 @@
 #include "tensor-internals/internal_expression.hpp"
 #include "tensor-internals/internal_buffer.hpp"
 #include "tensor-internals/functions/internal_function_linear.h"
+#include "tensor-internals/functions/internal_functions_inplace.h"
 
 namespace net::function {
 
@@ -12,6 +13,10 @@ Tensor linear(const Tensor& input, const Tensor& weight, const Tensor& bias) {
     internal::Buffer::instance() << expression;
     Tensor result(expression->perform());
     return result;
+}
+
+void softmax(Tensor& input, int axis = 0) {
+    internal::softmax_inplace(input.internal(), axis);
 }
 
 }
