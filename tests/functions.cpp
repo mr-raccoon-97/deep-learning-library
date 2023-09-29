4710b4ede17deebf6e04e6942360629e5ec02aa8
Chris Mc <prince.chrismc@gmail.com>
2023-09-29 14:29:14 -0700

    missing auto

--- tests/functions.cpp
+++ tests/functions.cpp
@@ -38,7 +38,7 @@ TEST(functions, gradient) {
     [0, 0, 0, 5, 7, 9, 4, 5, 6, 4, 5, 6, ]
     [0, 2, 1, 1, ]
     */
-    constexpr expected_x = std::to_array({0, 34, 0, 0, 0, 79, 17, 27});
+    constexpr auto expected_x = std::to_array({0, 34, 0, 0, 0, 79, 17, 27});
     std::vector<int> diff_x;
  
     std::set_difference(expected_x.begin(), expected_x.end(), x.begin(), x.end(),
