7e1099505864abfb0009e5127299137a94ce96a7
Chris McArthur <prince.chrismc@gmail.com>
2023-10-16 12:54:35 -0700

    switch optimizers to shared ptr

--- include/CaberNet/model.h
+++ include/CaberNet/model.h
@@ -10,10 +10,6 @@ namespace net {
 
 template<class Derived>
 class Model {
-    using optimizer_variant = std::variant<
-        std::shared_ptr<optimizer::SGD>
-    >;
-
     public:
     using size_type = std::size_t;
     using shape_type = std::vector<size_t>;
@@ -22,13 +18,17 @@ class Model {
         return static_cast<Derived*>(this)->forward(input);
     }
 
-    void configure_optimizer(optimizer_variant instance) {
-        optimizer_ = std::visit([](auto&& argument) { return argument; }, instance);
-        static_cast<Derived*>(this)->set_optimizer(optimizer_);
+    void configure_optimizer(std::shared_ptr<net::base::Optimizer> instance) {
+        static_cast<Derived*>(this)->set_optimizer(instance);
+        optimizer_ = instance;
     }
 
     private:
     std::shared_ptr<net::base::Optimizer> optimizer_ = std::make_shared<net::optimizer::NoOptimization>();
+
+    protected:
+    Model() = default;
+    Model(std::shared_ptr<net::base::Optimizer> optimizer) : optimizer_(optimizer){    }
 };
 
 } // namespace net
